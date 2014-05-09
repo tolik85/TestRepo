85695bd37db396f1c725b84a44255d916e53f336
tolik85 <nathangurfnkel@Nathans-MacBook-Pro.local>
2014-05-10 00:26:27 +0300

    My second edit

--- SourceTreeTest/PXViewController.m
+++ SourceTreeTest/PXViewController.m
@@ -19,6 +19,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"this is a first edit");
+    NSLog(@"this is a second edit");
+    
 }
 
 - (void)didReceiveMemoryWarning
